6e96257cca437c17cc0c6b62ba21d0192ebdc890
Stift007 <74728802+Stift007@users.noreply.github.com>
2022-04-27 14:40:41 +0200

    Create app.py

---
+++ aurelia/app.py
@@ -0,0 +1,45 @@
+import http
+import http.server
+import http.client
+from math import inf
+import response
+from config import Aurelia
+
+class Requesthandler(http.server.BaseHTTPRequestHandler):
+    
+    def do_GET(self):
+        try:
+            rule, rname = self.find(lambda r:r[0] == self.path,self.aurel.routes)
+            res = self.aurel.views[rname](self)
+            if isinstance(res,response.Response):
+                self.send_response(res.status)
+                self.send_header("Content-Type", res.mimetype)
+                for i in res.headers:
+                    self.send_header(i[0],i[1])
+                self.end_headers( )
+                if isinstance(res.body,bytes):
+                    self.wfile.write(res.body)
+                else:
+                    self.wfile.write(res.body.encode())
+            else:
+                self.send_response(200)
+                self.send_header("Content-Type", "text/plain")
+                self.end_headers( )
+                self.wfile.write(res.encode())
+            
+        except Exception as e:
+            return self.wfile.write(str(e.with_traceback(e.__traceback__)).encode())
+
+    def find(self,predicate,iterable):
+        for i in iterable:
+            if predicate(i): 
+                return i
+        return None
+
+def run(host="127.0.0.1",port=5000,config:Aurelia=None):
+    Handle = Requesthandler
+    Handle.aurel = config
+    with http.server.HTTPServer((host,port), Handle) as https:
+        print(f"Running app '{__name__}' on http://{host}:{port}")        
+        while True:
+            https.handle_request()
