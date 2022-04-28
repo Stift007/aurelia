698b059ad6e13dc6b049f8f815517c9baa5893b4
Stift007 <74728802+Stift007@users.noreply.github.com>
2022-04-28 08:40:07 +0200

    Update config.py

--- aurelia/config.py
+++ aurelia/config.py
@@ -1,23 +1,22 @@
-import typing
-
 class Aurelia:
     def __init__(self):
         self.routes = []
         self.views = {}
-        self.methods = {}
+        self.on_before_request = None
 
-    def register_route(self,rule,route_name,methods:typing.List[str]=["GET"]):
+    def register_route(self,rule,route_name):
         self.routes.append((rule,route_name))
-        self.methods[route_name] = methods
 
     def add_view(self,function,route_name):
         self.views[route_name] = function
 
-    def view(self,path,**kwds):
+    def view(self,path):
         def predicate(function):
             route_name = function.__name__
             self.register_route(path,route_name)
             self.add_view(function,route_name)
-            self.methods[route_name] = kwds.get("request_method",["GET"])
 
         return predicate
+
+    def set_before_request(self,requesthandle):
+        self.on_before_request = requesthandle
