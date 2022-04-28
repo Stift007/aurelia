659c61bbd1a231107058ce86dfd197ecda8a7f0b
Stift007 <74728802+Stift007@users.noreply.github.com>
2022-04-28 08:40:32 +0200

    Update response.py

--- aurelia/response.py
+++ aurelia/response.py
@@ -1,6 +1,13 @@
+from typing import List
+import mimetypes
+
 class Response:
     def __init__(self,*body,mimetype="text/plain",status:int=200,headers=[]) -> None:
-        self.body = " ".join(body)
+        self.body = " ".join(body) if isinstance(body[0], str) else body
         self.mimetype = mimetype
         self.status = status
         self.headers = headers
+
+class FileResponse(Response):
+    def __init__(self, fp, status: int = 200, headers=[]) -> None:
+        super().__init__(open(fp,"rb").read(),mimetype=mimetypes.guess_type(fp),status=status, headers=headers)
